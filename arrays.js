fba53b1d77432873c3946ea370d8fbb239a5ad7a
Yohannes Abera <yohannesns@gmail.com>
2018-05-27 21:22:45 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,42 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittle"]
+
+function addElementToBeginningOfArray(array,element){
+ return  [element,...array]
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+   array.unshift(element)
+   return array
+}
+ // function addElementToEndOfArray(array,element){
+ //   [array...,element]
+ //
+ // }
+//  function destructivelyAddElementToEndOfArray(array,element){
+//    array.push(element)
+//
+// }
+ function accessElementInArray(array,index){
+   return array[index];
+    }
+function destructivelyRemoveElementFromBeginningOfArray(array,element){
+   array.shift(element)
+
+
+ }
+ function removeElementFromBeginningOfArray(array,element){
+     array.slice(1,array.length-1)
+ 
+
+
+ }
+function destructivelyRemoveElementFromEndOfArray(array,element){
+  array.pop(element)
+  return array
+
+ }
+ function removeElementFromEndOfArray(array,element){
+  return array.slice(0, array.length - 1)
+
+
+
+ }
