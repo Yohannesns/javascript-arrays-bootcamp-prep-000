b510b4e65149933ddbc9fbe266e0cb0ad7e5c805
Yohannes Abera <yohannesns@gmail.com>
2018-05-27 21:36:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ function destructivelyAddElementToBeginningOfArray(array,element){
    return array
 }
  function addElementToEndOfArray(array,element){
-    [array,...element]
+    [array, ...element]
      return array
 
  }
